e91de479a883782f51a6972ae684d5845972279f
Igor Chorazewicz <chorig9@gmail.com>
2019-12-07 17:56:02 +0100

    cleanup

--- stage1/simulate.hpp
+++ stage1/simulate.hpp
@@ -142,36 +142,10 @@ void collide(float &x1, float &y1, float m1, float &v_x1, float &v_y1, float &x2
     v_y2 -= (2 * m1) / (m1 + m2) * dot2 / norm * (y2 - y1);
 }
 
-void collide2(float &x1, float &y1, float m1, float &v_x1, float &v_y1, float &x2, float& y2, float m2, float &v_x2, float &v_y2)
-{
-    float overlap_x = x2 - x1;
-    float overlap_y = y2 - y1;
-
-    float overlap_dist = sqrt(overlap_x * overlap_x + overlap_y * overlap_y);
-    float radius_sum = m1 + m2;
-    float coef = radius_sum / overlap_dist;
-
-    /* move x2 away from x1 so that x2 - x1 == r1 + r2 */
-    x2 = x1 + coef * overlap_x;
-    y2 = y1 + coef * overlap_y;
-
-    auto norm = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
-    norm = norm != 0 ? norm : 1;
-
-    auto dot1 = (v_x1 - v_x2) * (x1 - x2) + (v_y1 - v_y2) * (y1 - y2);
-    auto dot2 = (v_x2 - v_x1) * (x2 - x1) + (v_y2 - v_y1) * (y2 - y1);
-
-    v_x1 -= (2 * m2) / (m1 + m2) * dot1 / norm * (x1 - x2);
-    v_y1 -= (2 * m2) / (m1 + m2) * dot1 / norm * (y1 - y2);
-
-    v_x2 -= (2 * m1) / (m1 + m2) * dot2 / norm * (x2 - x1);
-    v_y2 -= (2 * m1) / (m1 + m2) * dot2 / norm * (y2 - y1);
-}
-
 class simulation
 {
 public:
-    simulation(int n_balls, int seed): _balls(n_balls)//, _collided_1(n_balls), _collided_2(n_balls)
+    simulation(int n_balls, int seed): _balls(n_balls)
     {
         srand(seed);
 
@@ -225,29 +199,6 @@ public:
                 collisions_num[i] = 0;
             }
 
-            // int threadid = omp_get_thread_num();
-            // int threadcount = omp_get_num_threads();
-            // int num_per_thread = _balls.size / threadcount;
-
-            // for (int i = 0; i < _balls.size; i++) {
-            //     int region_x = (_balls.x[i] + WIDTH / 2) / REGIONS_SINGLE;
-            //     int region_y = (_balls.y[i] + HEIGHT / 2) / REGIONS_SINGLE;
-
-            //     int r = region_x + region_y * REGIONS_X;
-
-            //     if (r >= threadid * num_per_thread && r < (threadid + 1) * num_per_thread)
-            //         {
-            //             auto *balls = &regions[r];
-            //             balls->x[balls->size] = _balls.x[i];
-            //             balls->y[balls->size] = _balls.y[i];
-            //             balls->v_x[balls->size] = _balls.v_x[i];
-            //             balls->v_y[balls->size] = _balls.v_y[i];
-            //             balls->mass[balls->size] = _balls.mass[i];
-            //             balls->index[balls->size] = _balls.index[i];
-            //             balls->size++;
-            //         }
-            // }
-
             #pragma omp for
             for (int i = 0; i < _balls.size; i++) {
                 Balls* balls;
@@ -270,137 +221,6 @@ public:
                 balls->mass[in] = _balls.mass[i];
                 balls->index[in] = _balls.index[i];
             }
-
-            // #pragma omp for
-            // for (int i = 0; i < _balls.size; i+= 8) {
-            //     Balls* balls[8];
-
-            //     for (int j = 0; j < 8; j++) {
-            //         int region_x = (_balls.x[i + j] + WIDTH / 2) / REGIONS_SINGLE;
-            //         int region_y = (_balls.y[i + j] + HEIGHT / 2) / REGIONS_SINGLE;
-
-            //         int r = region_x + region_y * REGIONS_X;
-            //         balls[j] = &regions[r];
-            //     }
-
-            //     #pragma omp critical
-            //     {
-            //         for (int j = 0; j < 8; j++) {
-            //             balls[j]->x[balls[j]->size] = _balls.x[i + j];
-            //             balls[j]->y[balls[j]->size] = _balls.y[i + j];
-            //             balls[j]->v_x[balls[j]->size] = _balls.v_x[i+ j ];
-            //             balls[j]->v_y[balls[j]->size] = _balls.v_y[i+ j];
-            //             balls[j]->mass[balls[j]->size] = _balls.mass[i+ j];
-            //             balls[j]->index[balls[j]->size] = _balls.index[i+ j];
-            //             balls[j]->size++;
-            //         }
-            //     }
-            // }
-
-            // #pragma omp for
-            // for (int r = 0; r < REGIONS_NUM; r++) {
-            //     int neighbour_regions[] = {1, REGIONS_X, REGIONS_X + 1};
-
-            //     // neighbour regions
-            //     for (int rs = 0; rs < 3; rs++) {
-            //         int neighbour = r + neighbour_regions[rs];
-
-            //         if (neighbour < 0 || neighbour >= REGIONS_NUM)
-            //             continue;
-
-            //         for (int i = 0; i < regions[r].size; i++) {
-            //             for (int j = 0; j < regions[neighbour].size; j++) {
-            //                 if (is_collision(regions[r].x[i],
-            //                         regions[r].y[i],
-            //                         regions[r].mass[i],
-            //                         regions[neighbour].x[j],
-            //                         regions[neighbour].y[j],
-            //                         regions[neighbour].mass[j])) {
-            //                             auto &num = collisions_num[regions[r].index[i]];
-            //                             collisions[num][regions[r].index[i]] = regions[neighbour].index[j];
-            //                             num++;
-            //                         }
-            //             }
-            //         }
-            //     }
-
-            //     for (int i = 0; i < regions[r].size; i++) {
-            //         for (int j = i + 1; j < regions[r].size; j++) {
-            //                 if (is_collision(regions[r].x[i],
-            //                         regions[r].y[i],
-            //                         regions[r].mass[i],
-            //                         regions[r].x[j],
-            //                         regions[r].y[j],
-            //                         regions[r].mass[j])) {
-            //                             auto &num = collisions_num[regions[r].index[i]];
-            //                             collisions[num][regions[r].index[i]] = regions[r].index[j];
-            //                             num++;
-            //                 }
-            //         }
-            //     }
-            // }
-
-            // for (int i = 0; i < MAX_COLLISIONS; i++)
-            // {
-            //     Balls collided_1(_balls.size);
-            //     Balls collided_2(_balls.size);
-            //     int size = 0;
-
-            //     #pragma omp for
-            //     for (int j = 0; j < _balls.size; j++) {
-            //         if (collisions_num[j] > i) {
-            //             collided_1.x[size] = _balls.x[j];
-            //             collided_1.y[size] = _balls.y[j];
-            //             collided_1.v_x[size] = _balls.v_x[j];
-            //             collided_1.v_y[size] = _balls.v_y[j];
-            //             collided_1.mass[size] = _balls.mass[j];
-            //             collided_1.index[size] = _balls.index[j];
-
-            //             collided_2.x[size] = _balls.x[collisions[i][j]];
-            //             collided_2.y[size] = _balls.y[collisions[i][j]];
-            //             collided_2.v_x[size] = _balls.v_x[collisions[i][j]];
-            //             collided_2.v_y[size] = _balls.v_y[collisions[i][j]];
-            //             collided_2.mass[size] = _balls.mass[collisions[i][j]];
-            //             collided_2.index[size] = _balls.index[collisions[i][j]];
-            //             size++;
-            //         }
-            //     }
-
-            //     #pragma omp for
-            //     for (int j = 0; j < size; j++) {
-            //             collide2(collided_1.x[j],
-            //                         collided_1.y[j],
-            //                         collided_1.mass[j],
-            //                         collided_1.v_x[j],
-            //                         collided_1.v_y[j],
-            //                         collided_2.x[j],
-            //                         collided_2.y[j],
-            //                         collided_2.mass[j],
-            //                         collided_2.v_x[j],
-            //                         collided_2.v_y[j]);
-            //     }
-
-            //     #pragma omp for
-            //     for (int j = 0; j < size; j++) {
-            //             auto index = collided_1.index[j];
-
-            //             _balls.x[index] = collided_1.x[j];
-            //             _balls.y[index] = collided_1.y[j];
-            //             _balls.v_x[index] = collided_1.v_x[j];
-            //             _balls.v_y[index] = collided_1.v_y[j];
-            //             _balls.mass[index] = collided_1.mass[j];
-            //             _balls.index[index] = collided_1.index[j];
-
-            //             index = collided_2.index[j];
-
-            //             _balls.x[index] = collided_2.x[j];
-            //             _balls.y[index] = collided_2.y[j];
-            //             _balls.v_x[index] = collided_2.v_x[j];
-            //             _balls.v_y[index] = collided_2.v_y[j];
-            //             _balls.mass[index] = collided_2.mass[j];
-            //             _balls.index[index] = collided_2.index[j];
-            //     }
-            // }
 
             #pragma omp for collapse(2)
             for (int r1 = 0; r1 < REGIONS_X; r1 +=2) {
@@ -541,5 +361,4 @@ private:
     int *collisions_num;
     int x_regions, y_regions;
     Balls _balls;
-   // Balls _collided_1, _collided_2;
 };
